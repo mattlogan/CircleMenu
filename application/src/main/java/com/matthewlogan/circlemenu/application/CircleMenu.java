dd4e50efa0c24f0e6be761347eb48689dfffd419
mattlogan <matt.b.logan@gmail.com>
2014-03-08 22:46:47 -0800

    initial commit

---
+++ application/src/main/java/com/matthewlogan/circlemenu/application/CircleMenu.java
@@ -0,0 +1,131 @@
+package com.matthewlogan.circlemenu.application;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.FrameLayout;
+import android.widget.ListView;
+
+public class CircleMenu extends FrameLayout
+        implements AdapterView.OnItemClickListener, Animation.AnimationListener {
+
+    private Context mContext;
+
+    private ListView mListView;
+
+    private OnItemClickListener mListener;
+
+    private Animation mInitialHideAnimation;
+    private Animation mHideAnimation;
+    private Animation mShowAnimation;
+
+    private boolean mIsAnimating = false;
+    private boolean mIsShowing = true;
+
+    // This is the ListView's listener.  We'll use this to trigger our own.
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        mListener.onItemClick(position - 1);
+    }
+
+    // Whoever is implementing this class only needs to know the clicked item position.
+    public interface OnItemClickListener {
+        public void onItemClick(int position);
+    }
+
+    public CircleMenu(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        mContext = context;
+
+        setupMenu();
+        loadAnimations();
+
+        // The view loads in it's "showing" position, and we animate it away instantaneously
+        // before the view becomes visible.
+        startAnimation(mInitialHideAnimation);
+    }
+
+    private void setupMenu() {
+        mListView = new ListView(mContext);
+        mListView.setLayoutParams(new ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        mListView.setOnItemClickListener(this);
+        addView(mListView);
+    }
+
+    private void loadAnimations() {
+        mInitialHideAnimation = AnimationUtils.loadAnimation(mContext,
+                R.anim.circle_menu_initial_hide);
+        mHideAnimation = AnimationUtils.loadAnimation(mContext, R.anim.circle_menu_hide);
+        mShowAnimation = AnimationUtils.loadAnimation(mContext, R.anim.circle_menu_show);
+
+        mInitialHideAnimation.setAnimationListener(this);
+        mHideAnimation.setAnimationListener(this);
+        mShowAnimation.setAnimationListener(this);
+    }
+
+    @Override
+    public void onAnimationStart(Animation animation) {
+        mIsAnimating = true;
+    }
+
+    @Override
+    public void onAnimationEnd(Animation animation) {
+        mIsAnimating = false;
+        mIsShowing = !mIsShowing;
+        setVisibility(mIsShowing ? View.VISIBLE : View.GONE);
+    }
+
+    @Override
+    public void onAnimationRepeat(Animation animation) {
+
+    }
+
+    public void setItems(String[] items) {
+        // Rather than dealing with adding a header view to the ListView, we'll just add an
+        // empty row.  This way, we don't have to inflate another view manually.
+        String[] itemsWithHeader = new String[items.length + 1];
+        for (int i = 0; i < items.length + 1; i++) {
+            itemsWithHeader[i] = (i == 0) ? "" : items[i - 1];
+        }
+
+        CircleMenuAdapter<String> adapter = new CircleMenuAdapter<String>(mContext,
+                R.layout.circle_menu_item, R.id.circle_menu_item_text, itemsWithHeader);
+        mListView.setAdapter(adapter);
+    }
+
+    public void setOnItemClickListener(OnItemClickListener listener) {
+        mListener = listener;
+    }
+
+    public void toggle() {
+        if (mIsAnimating) {
+            return;
+        }
+
+        startAnimation(mIsShowing ? mHideAnimation : mShowAnimation);
+    }
+
+    private class CircleMenuAdapter<String> extends ArrayAdapter<String> {
+
+        public CircleMenuAdapter(Context context, int resource, int textViewResourceId,
+                                 String[] objects) {
+
+            super(context, resource, textViewResourceId, objects);
+        }
+
+        // Makes the top row (presumably underneath an action bar or control of some kind)
+        // not clickable.
+        @Override
+        public boolean isEnabled(int position) {
+            return !(position == 0);
+        }
+    }
+
+}
---
+++ application/src/main/java/com/matthewlogan/circlemenu/application/MainActivity.java
@@ -0,0 +1,74 @@
+package com.matthewlogan.circlemenu.application;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends Activity
+        implements CircleMenu.OnItemClickListener, View.OnClickListener {
+
+    private int mCurrentPosition;
+
+    private int[] mBackgroundImages = new int[] {
+            R.drawable.hawk_landing_on_post,
+            R.drawable.walla_walla_skateboarder,
+            R.drawable.windmills
+    };
+
+    private String[] mBackgroundImageNames = new String[] {
+            "Hawk Landing on Post",
+            "Walla Walla Skateboarder",
+            "Windmills"
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getMenuButton().setOnClickListener(this);
+
+        getCircleMenu().setOnItemClickListener(this);
+        getCircleMenu().setItems(mBackgroundImageNames);
+
+        if (savedInstanceState != null) {
+            setBackgroundImage(savedInstanceState.getInt("currentPosition"));
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putInt("currentPosition", mCurrentPosition);
+    }
+
+    // Menu item click
+    @Override
+    public void onItemClick(int position) {
+        setBackgroundImage(position);
+    }
+
+    // Menu toggle click
+    @Override
+    public void onClick(View v) {
+        getCircleMenu().toggle();
+    }
+
+    private void setBackgroundImage(int position) {
+        getBackgroundImage().setImageDrawable(
+                getResources().getDrawable(mBackgroundImages[position]));
+        mCurrentPosition = position;
+    }
+
+    private View getMenuButton() {
+        return findViewById(R.id.menu_button);
+    }
+
+    private CircleMenu getCircleMenu() {
+        return (CircleMenu) findViewById(R.id.circle_menu);
+    }
+
+    private ImageView getBackgroundImage() {
+        return (ImageView) findViewById(R.id.background_image);
+    }
+}
