b9909d464a1134a11930a4a1e3abfcc2acd4326d
mattlogan <matt.b.logan@gmail.com>
2014-03-16 13:33:12 -0700

    Removed support library dependency

--- sample/src/main/java/com/matthewlogan/circlemenu/application/MainActivity.java
+++ sample/src/main/java/com/matthewlogan/circlemenu/application/MainActivity.java
@@ -3,6 +3,7 @@ package com.matthewlogan.circlemenu.application;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.ImageView;
 
 import com.matthewlogan.circlemenu.library.CircleMenu;
@@ -28,6 +29,7 @@ public class MainActivity extends Activity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
 
         getMenuButton().setOnClickListener(this);
